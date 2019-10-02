b5debd9cbf1bcf6a937f5287c0fbd7497b2b823c
evansibok <evansibok@gmail.com>
2019-10-02 13:40:58 +0100

    added a useEffect to the useDarkMode custom hook

--- src/components/hooks/useDarkMode.js
+++ src/components/hooks/useDarkMode.js
@@ -1,4 +1,13 @@
+import { useEffect } from "react";
+import { useLocalStorage } from "./useLocalStorage";
 
-export const useDarkMode = () => {
-  
+export const useDarkMode = (key, initialValue) => {
+  const [darkModeActive, setDarkModeActive] = useLocalStorage(key, initialValue);
+
+  useEffect(() => {
+    if(darkModeActive){
+      return style={body: "dark-mode"}
+    }
+    return style ={body: initialValue};
+  }, [darkModeActive])
 }
