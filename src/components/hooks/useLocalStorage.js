c173c73c0533e63bc162078a9c614bc143fdd944
Evans Ibok <evansibok@gmail.com>
2019-12-04 14:37:40 +0100

    initialised state for value retrieval

--- src/components/hooks/useLocalStorage.js
+++ src/components/hooks/useLocalStorage.js
@@ -1,5 +1,15 @@
 import { useState } from 'react'
 
-export const useLocalStorage = () => {
+export const useLocalStorage = (key, initialValue) => {
+	// Initial State to store value
+	const [storedValue, setStoredValue] = useState(() => {
+		// Get the stored value from localStorage by key
+		const currentItem = window.localStorage.getItem(key);
+		// if currentItem exists return Parsed json otherwise return initialValue
+		return currentItem ? JSON.parse(currentItem) : initialValue;
+	});
+
+
+	return [storedValue, setStoredValue]
 
 }
