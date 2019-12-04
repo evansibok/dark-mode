f5c247b914ddf3521242f11f1745d28ef330dd4a
Evans Ibok <evansibok@gmail.com>
2019-12-04 13:04:16 +0100

    gotten the dark mode to work on toggle but not yet persisted

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Navbar = () => {
   const [darkMode, setDarkMode] = useState(false);
+
+  const darkFunction = () => {
+    darkMode
+      ? document.querySelector("body").classList.remove("dark-mode")
+      : document.querySelector("body").classList.add("dark-mode");
+  };
+
   const toggleMode = e => {
     e.preventDefault();
     setDarkMode(!darkMode);
+    darkFunction();
   };
+
   return (
     <nav className="navbar">
       <h1>Crypto Tracker</h1>
       <div className="dark-mode__toggle">
         <div
           onClick={toggleMode}
-          className={darkMode ? 'toggle toggled' : 'toggle'}
+          className={darkMode ? "toggle toggled" : "toggle"}
         />
       </div>
     </nav>
