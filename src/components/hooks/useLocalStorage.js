cd1f0b0e0ba8c8741617c4de252d5ad0d33b2018
Evans Ibok <evansibok@gmail.com>
2019-12-05 16:42:37 +0100

    minor change in useDarkMode to make the hook reusable

--- src/components/hooks/useLocalStorage.js
+++ src/components/hooks/useLocalStorage.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-export const useLocalStorage = (key, initialValue) => {
+export const useLocalStorage = (key, initialValue = "") => {
 	// Initial State to store value
 	const [storedValue, setStoredValue] = useState(() => {
 		// Get the stored value from localStorage by key
@@ -13,12 +13,10 @@ export const useLocalStorage = (key, initialValue) => {
 	// Return a wrapped version of useState's setter function that ...
 	// ... persists the new value to localStorage.
 	const setValue = value => {
-		// Allow value to be a function so we have same API as state
-		const valueSaved = value instanceof Function ? value(storedValue) : value;
 		// save the value to state
-		setStoredValue(valueSaved)
+		setStoredValue(value)
 		// save the value stringified to localStorage
-		window.localStorage.setItem(key, JSON.stringify(valueSaved))
+		window.localStorage.setItem(key, JSON.stringify(value))
 	}
 
 	return [storedValue, setValue];
